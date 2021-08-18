0e2a95e785b0e1dc07b79c5534ad37bedabd22ff
nidhallakdhar <80424590+nidhallakdhar@users.noreply.github.com>
2021-08-18 11:36:44 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php
+<?
 $VotreAdresseMail="nimacom2021@gmail.com";//mettez ici votre adresse mail
 if(isset($_POST['envoyer'])) { // si le bouton "Envoyer" est appuyé
     //on vérifie que le champ mail est correctement rempli
